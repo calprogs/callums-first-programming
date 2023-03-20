9de58652ba5d76eaded30a298b795e9e4cc0e49b
calprogs <120546681+calprogs@users.noreply.github.com>
2023-03-19 21:10:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P9, 180)
-    music.playMelody("G F G A - F E D ", 120)
+    basic.showIcon(IconNames.Angry)
+    pins.servoWritePin(AnalogPin.P9, 0)
+    pins.servoWritePin(AnalogPin.P8, 180)
+    music.playMelody("B A G A G F A C5 ", 120)
 })
 input.onButtonPressed(Button.B, function () {
-    music.playMelody("C5 G B A F A C5 B ", 120)
-    pins.servoWritePin(AnalogPin.P9, 0)
+    basic.showIcon(IconNames.Yes)
+    pins.servoWritePin(AnalogPin.P8, 0)
+    pins.servoWritePin(AnalogPin.P9, 180)
+    music.playMelody("E D G F B A C5 B ", 120)
 })
 makerbit.connectLcd(39)
-makerbit.showStringOnLcd1602("Callum's Box", makerbit.position1602(LcdPosition1602.Pos1), 16)
+makerbit.showStringOnLcd1602("Callum's box", makerbit.position1602(LcdPosition1602.Pos1), 16)
+makerbit.showStringOnLcd1602("yes no", makerbit.position1602(LcdPosition1602.Pos17), 16, TextOption.AlignLeft)
 basic.forever(function () {
 	
 })
